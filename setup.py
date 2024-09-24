e6cf548983855e3dad0e6c69c3e3ceb08d91ae5a
marcosdaniel2002 <mteranc@unemi.edu.ec>
2024-09-24 12:29:58 -0500

    cambiar a v 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-audit-kafkalogger',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'Django>=3.0',
