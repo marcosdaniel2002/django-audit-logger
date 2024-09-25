fbc50570d0014716c0e372c9b3e1a1b2d82819a5
marcosdaniel2002 <mteranc@unemi.edu.ec>
2024-09-24 19:57:52 -0500

    Now Django>=2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unemi-audi-kafka',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     install_requires=[
         'Django>=2.2',
