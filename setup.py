e8bffdcfbc471c4af72c5b5d474e216bb02e223e
marcosdaniel2002 <mteranc@unemi.edu.ec>
2024-09-24 21:01:32 -0500

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unemi-audi-kafka',
-    version='0.9',
+    version='0.10',
     packages=find_packages(),
     install_requires=[
         'Django>=2.2',
