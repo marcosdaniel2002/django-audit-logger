7d6502d94cbcaa503fc0591c4b35a76bfe76752f
mteranc <mteranc@unemi.edu.ec>
2024-10-10 12:59:45 -0500

    aceptar audit_config_models.py para auditoria tablas de configuracion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unemi-audi-kafka',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     install_requires=[
         'Django>=2.2',
